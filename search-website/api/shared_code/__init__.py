1e8f71a790a56a90c6a2477f41d362ba0380b610
fzarate64 <fzarate@ultracom.com.co>
2022-03-04 19:56:24 -0500

    Update __init__.py

--- search-website/api/shared_code/__init__.py
+++ search-website/api/shared_code/__init__.py
@@ -4,8 +4,8 @@ def azure_config():
     
     configs={}
     configs["search_facets"]= "authors*,language_code"
-    configs["search_index_name"]=os.environ.get("SearchIndexName","")
-    configs["search_service_name"]=os.environ.get("SearchServiceName","")
-    configs["search_api_key"]=os.environ.get("SearchApiKey","")
-    
+    configs["search_index_name"]=os.environ.get("SearchIndexName","good-books")
+    configs["search_service_name"]=os.environ.get("SearchServiceName","servicio-busqueda")
+    configs["search_api_key"]=os.environ.get("SearchApiKey","40410AD301192C5C71231411E593477D")
+        
     return configs
